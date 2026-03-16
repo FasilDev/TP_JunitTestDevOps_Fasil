8db151ef80c65007efe96df0dd6a772a6f6e0e28
Fasil Mougamadou <mougamadou.fasil@gmail.com>
2026-03-16 15:11:59 +0100

    Ajout de la classe ArticleTest : Ajout - Test assertEquals (Vérification : réference, nom et prix) + (Vérification si le prix à bien été modifié)

---
+++ src/test/ArticleTest.java
@@ -0,0 +1,28 @@
+package test;
+import code.Article;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArticleTest {
+
+    @Test
+    void creerArticleValideDoitFonctionner() {
+        // Arranger & Agir
+        Article article = new Article("REF-001", "Cahier", 3.50);
+        // Affirmer
+        assertEquals("REF-001", article.getReference());
+        assertEquals("Cahier", article.getNom());
+        assertEquals(3.50, article.getPrix(), 0.001);
+
+    }
+
+    @Test
+    void modifierPrixDoitMettreAJourLePrix() {
+        //Arranger
+        Article article = new Article("REF-002", "Règle", 1.00);
+        //Agir
+        article.setPrix(1.50);
+        //Affirmer
+        assertEquals(1.50, article.getPrix(), 0.001);
+    }
+}
