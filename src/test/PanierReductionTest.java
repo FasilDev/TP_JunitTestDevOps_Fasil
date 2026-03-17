fe79dd2a4d8c370af3da7285bfa22f78d516fefe
Fasil Mougamadou <mougamadou.fasil@gmail.com>
2026-03-17 15:49:11 +0100

    Création d'un DockerFile

--- src/test/PanierReductionTest.java
+++ src/test/PanierReductionTest.java
@@ -10,15 +10,16 @@ class PanierReductionTest {
     @ParameterizedTest
     @CsvSource({
             " , 100.0", // pas de code de réduction
-            "REDUC10, 90.0", // -10%
-            "REDUC20, 80.0" // -20%
+            "reduc10, 90.0", // -10%
+            "reduc20, 80.0" // -20%
     })
     void calculerTotalDoitAppliquerLaBonneReduction(
             String code, double totalAttendu) {
         // Arranger
         Panier panier = new Panier();
         Article article = new Article("REF-001", "Classeur", 10.0);
-        panier.ajouterArticle(article, 10); // sous-total = 100.0
+        panier.ajouterArticle(article, 9); // sous-total = 90.0
+
         // Agir
         if (code != null && !code.isBlank()) {
             panier.appliquerCodeReduction(code.trim());
